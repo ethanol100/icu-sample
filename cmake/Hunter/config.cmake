0db267c85231b2846d9a371e5badc4710691ef97
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-05-09 13:52:13 +0300

    Build with U_USING_ICU_NAMESPACE=ON

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,10 @@
+set(icu_args U_USING_ICU_NAMESPACE=ON)
 if(ARCHIVE_MODE)
-  hunter_config(
-      ICU
-      VERSION ${HUNTER_ICU_VERSION}
-      CMAKE_ARGS ICU_DATA_ARCHIVE_MODE=ON
-  )
+  list(APPEND icu_args ICU_DATA_ARCHIVE_MODE=ON)
 endif()
+
+hunter_config(
+    ICU
+    VERSION ${HUNTER_ICU_VERSION}
+    CMAKE_ARGS ${icu_args}
+)
